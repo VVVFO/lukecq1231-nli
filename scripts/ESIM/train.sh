834a780744d23586f02b3f04664594927c9d60c1
VVVFO <yaoqingning@gmail.com>
2018-04-17 00:39:54 -0400

    Finally got GPU working :(

--- scripts/ESIM/train.sh
+++ scripts/ESIM/train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # GPU
-export THEANO_FLAGS='mode=FAST_RUN,device=gpu0,floatX=float32,optimizer_including=cudnn,warn_float64=warn,lib.cnmem=0.9'
+export THEANO_FLAGS='mode=FAST_RUN,device=gpu0,floatX=float32,optimizer_including=cudnn,warn_float64=warn,lib.cnmem=0.9,nvcc.flags=-D_FORCE_INLINES'
 
 # CPU
 # export THEANO_FLAGS='mode=FAST_RUN,device=cpu,floatX=float32'
